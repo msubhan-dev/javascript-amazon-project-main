aebe6b192dac953b26210fcc81a625e196d740b0
SubhanDev <mt694222@gmail.com>
2026-09-06 15:41:49 +0500

    fixing errors and Summary of order and payment

--- data/cart.js
+++ data/cart.js
@@ -1,65 +1,67 @@
 export let cart = JSON.parse(localStorage.getItem('cart'));
 
-if(!cart){
-    cart = [{
-                productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-                quantity: 2,
-                deliveryOptionId: '1'
-            }, {
-                productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
-                quantity: 1,
-                deliveryOptionId: '2'
-            }];
+if (!cart) {
+  cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2,
+    deliveryOptionId: '1'
+  }, {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1,
+    deliveryOptionId: '2'
+  }];
 }
 
-
-
-
-// save in local stroge of data
-function saveToStroage(){
-    localStorage.setItem('cart', JSON.stringify(cart));
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
 }
 
+export function addToCart(productId) {
+  let matchingItem;
 
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
 
-// add to Cart products
-export function addToCart(productId) 
-        {   //working of cart button function
-            let matchingItem;
-
-            cart.forEach((CartItem) =>{
-                    if(productId === CartItem.productId){
-                        matchingItem = CartItem;        }
-                });
-                //
-                if(matchingItem){
-                    matchingItem.quantity += 1;
-                }else
-                    {
-                        cart.push({
-                            productId: productId,
-                            quantity: 1,
-                            deliveryOptionId: '1'
-                        });
-                    }
-                    saveToStroage();
-        }
-
-
-
-//[ removing || using delete button for crat summary ] 
-export function removeFromCart(productId){
-    const newCart = [];
-    cart.forEach((cartItem) => {
-        if(cartItem.productId !== productId){
-            newCart.push(cartItem);
-        }
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1,
+      deliveryOptionId: '1'
     });
+  }
 
-    cart = newCart;
-
-    saveToStroage();
+  saveToStorage();
 }
 
+export function removeFromCart(productId) {
+  const newCart = [];
 
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
 
+  cart = newCart;
+
+  saveToStorage();
+}
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.deliveryOptionId = deliveryOptionId;
+
+  saveToStorage();
+}
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,93 +1,81 @@
-/* import {cart as myCart} from '../data/cart.js';
-const cart = [];
- */
-import {cart,addToCart} from '../data/cart.js';
+import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
-import {formatCurrency} from './utils/money.js'
-
-
-console.log("Amazon script loaded");
+import {formatCurrency} from './utils/money.js';
 
 let productsHTML = '';
-/* creating html in js through this above data  */
+
 products.forEach((product) => {
-    productsHTML += `
+  productsHTML += `
     <div class="product-container">
-          <div class="product-image-container">
-            <img class="product-image"
-              src="${product.image}">
-          </div>
+      <div class="product-image-container">
+        <img class="product-image"
+          src="${product.image}">
+      </div>
 
-          <div class="product-name limit-text-to-2-lines">
-            ${product.name}
-          </div>
+      <div class="product-name limit-text-to-2-lines">
+        ${product.name}
+      </div>
 
-          <div class="product-rating-container">
-            <img class="product-rating-stars"
-              src="images/ratings/rating-${product.rating.stars*10}.png">
-            <div class="product-rating-count link-primary">
-              ${product.rating.count}
-            </div>)
-          </div>
-
-          <div class="product-price">
-            $${formatCurrency(product.priceCents)}
-          </div>
-
-          <div class="product-quantity-container">
-            <select>
-              <option selected value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-            </select>
-          </div>
-
-          <div class="product-spacer"></div>
-
-          <div class="added-to-cart">
-            <img src="images/icons/checkmark.png">
-            Added
-          </div>
-
-          <button class="add-to-cart-button button-primary
-          js-add-to-cart" data-product-id="${product.id}">
-            Add to Cart
-          </button>
+      <div class="product-rating-container">
+        <img class="product-rating-stars"
+          src="images/ratings/rating-${product.rating.stars * 10}.png">
+        <div class="product-rating-count link-primary">
+          ${product.rating.count}
         </div>
-    `;
-});
-//console.log(productsHTML);
-// that line used to show products on screen in innerHYML
-document.querySelector('.js-products-grid').innerHTML = productsHTML;
-// //function of update cart Quantity
-function updateCartQuantity(){
-            let cartQuantity = 0;
+      </div>
 
-                cart.forEach((cartItem) => {
-                    cartQuantity += Number(cartItem.quantity);
-                });
-                document.querySelector('.js-cart-quantity')
-                .innerHTML = cartQuantity;
-                console.log(cartQuantity);
-                console.log(cart); 
-        }
-// That handel the carts function 
+      <div class="product-price">
+        $${formatCurrency(product.priceCents)}
+      </div>
+
+      <div class="product-quantity-container">
+        <select>
+          <option selected value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+        </select>
+      </div>
+
+      <div class="product-spacer"></div>
+
+      <div class="added-to-cart">
+        <img src="images/icons/checkmark.png">
+        Added
+      </div>
+
+      <button class="add-to-cart-button button-primary js-add-to-cart"
+      data-product-id="${product.id}">
+        Add to Cart
+      </button>
+    </div>
+  `;
+});
+
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+function updateCartQuantity() {
+  let cartQuantity = 0;
+
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+
+  document.querySelector('.js-cart-quantity')
+    .innerHTML = cartQuantity;
+}
+
 document.querySelectorAll('.js-add-to-cart')
-    .forEach((button)=>{
-        button.addEventListener('click', () => {
-            //(that show the working )console.log('added ptoducts to cart');
-            const productId = button.dataset.productId;
-            //working of cart button call back function
-            addToCart(productId);
-            //function of update cart Quantity
-            updateCartQuantity();
-            
-        });
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+      const productId = button.dataset.productId;
+      addToCart(productId);
+      updateCartQuantity();
     });
+  });
