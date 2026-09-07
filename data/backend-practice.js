88af6ae14c054b816b292364081eda5c228756c2
SubhanDev <mt694222@gmail.com>
2026-09-07 18:36:21 +0500

    off backend for enable domains and hosting

--- data/backend-practice.js
+++ data/backend-practice.js
@@ -1,3 +1,5 @@
-const xhr = new XMLHttpRequest();
-xhr.open('GET', 'https:'https://127.0.0.1:5500/checkout.html');
+/* const xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://github.com/msubhan-dev/javascript-amazon-project-main/blob/main/amazon.html');
 xhr.send();
+//
+xhr.response */
