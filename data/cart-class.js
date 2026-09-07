d2c17fb8b8c1774c6c51929e029d0271581c99c0
SubhanDev <mt694222@gmail.com>
2026-09-07 16:58:00 +0500

    fix error and creating opps and classes

--- data/cart-class.js
+++ data/cart-class.js
@@ -1,15 +1,15 @@
 class Cart {
     cartItems;
-    localStorageKey;
+    #localStorageKey;
 
         constructor(localStorageKey){
             // localstorage set
-                this.localStorageKey = localStorageKey;
-                this.loadFromStorage();
+                this.#localStorageKey = localStorageKey;
+                this.#loadFromStorage();
         }
     
-    loadFromStorage(){
-            this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
+    #loadFromStorage(){
+            this.cartItems = JSON.parse(localStorage.getItem(this.#localStorageKey));
 
             if (!this.cartItems) {
                 this.cartItems = [{
@@ -25,7 +25,7 @@ class Cart {
         }// 
 
             saveToStorage() {
-                    localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+                    localStorage.setItem(this.#localStorageKey, JSON.stringify(this.cartItems));
                 }// 
             addToCart(productId) {
                 let matchingItem;
@@ -62,15 +62,15 @@ class Cart {
             updateDeliveryOption(productId, deliveryOptionId) {
                     let matchingItem;
 
-                    this.cartItems.forEach((cartItem) => {
-                if (productId === cartItem.productId) {
-                        matchingItem = cartItem;
-                }
-                    });
+                this.cartItems.forEach((cartItem) => {
+                    if (productId === cartItem.productId) {
+                            matchingItem = cartItem;
+                    }
+                });
 
-                        matchingItem.deliveryOptionId = deliveryOptionId;
+                matchingItem.deliveryOptionId = deliveryOptionId;
 
-                        this.saveToStorage();
+                this.saveToStorage();
             }//
 
 }
@@ -80,6 +80,7 @@ const businessCart = new Cart('cart-business');
 
 console.log(cart);
 console.log(businessCart);
+console.log(businessCart instanceof Cart);
 
 
 
